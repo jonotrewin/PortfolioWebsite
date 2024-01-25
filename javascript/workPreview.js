7c10299dd9ca23f9f077832aa8d8af19c990fd2f
jonotrewin <157524390+jonotrewin@users.noreply.github.com>
2024-01-25 21:51:21 +0100

    addedcartoonbase-redidflex

--- javascript/workPreview.js
+++ javascript/workPreview.js
@@ -8,7 +8,7 @@ let WPdescription = document.getElementById("homepage-workinfobox-description");
 
 let WPvideo = document.getElementById("homepage-workinfobox-video");
 
-let WPimage = document.getElementById("homepage-workinfobox-image");
+
 
 let WPlink = document.getElementById("homepage-workinfobox-link");
 
@@ -44,8 +44,8 @@ function OpenWindow(item)
 
     if(!item.hasVideo)
     {
-        WPimage.style.display = "block"; 
-        WPimage.src= item.imageSRC;
+     
+      
         WPvideo.style.display ="none"
        
     }
@@ -53,7 +53,7 @@ function OpenWindow(item)
     {
         WPvideo.style.display ="block";
         WPvideo.src = item.videoURL;
-        WPimage.style.display="none";
+       
 
     }
 
@@ -80,7 +80,7 @@ let illegalHarvest =
 {
     card: document.getElementById("illegalharvestcard"),
     title:"Illegal Harvest",
-    desc: "Illegal Harvest was created for the Grads in Gaming Rising Star Competition",
+    desc: "Illegal Harvest was created for the Grads in Gaming Rising Star Competition. In the space of a month, I was able to design, prototype, program and playtest this game design prototype. Additionally, I modelled all the characters (including the veggies). Other assets, such as the Truck and Police Boats were created by Ioanna Efthymiadou.",
     role: "Designer, Artist, Programmer",
     imageSRC:"./images/30birds.jpg",
     videoURL: "https://www.youtube.com/embed/4-WuU0gLYck?si=uwJeinu4I6SZN98C",
@@ -98,7 +98,7 @@ let thirtyBirds =
 {
     card: document.getElementById("30birdscard"),
     title:"30 Birds",
-    desc: "30 Birds is the tale of detective Zig and her sidekick Hoop the Hoopoe, exploring a dangerous world in order to solve the case of a missing bird goddess. As narrative designer, I helped create the overarching plot, as well as writing the hilarious, strange characters and dialogue.",
+    desc: "30 Birds is the tale of detective Zig and her sidekick Hoop the Hoopoe, exploring a dangerous world in order to solve the case of a missing bird goddess. As narrative designer, I helped create the overarching plot, as well as writing the hilarious, strange characters and dialogue. Part of this work includes working with json and Twine files.",
     role: "Narrative Designer",
 
     imageSRC: "",
@@ -119,7 +119,7 @@ let supermarket =
 {
     card: document.getElementById("supermarketcard"),
     title:"Supermarket",
-    desc: "Supermarket is a political narrative created for the 'F#$* Captialism' gamejam on itch.io.\n\nIt was created with Zain Al Rubaie on Sound Design and Ioanna Efthymiadou as art designer and additional programmer. The Game Design was collaborated between the three of us. \n\n Note 25/01/2024: this link leads to an old version. The actual final version is being polished and will be updated ASAP.\n\n Password: gooseberry",
+    desc: "Supermarket is a political narrative game created for the 'F#$* Captialism' gamejam on itch.io.\n\nIt was created with Zain Al Rubaie on Sound Design and Ioanna Efthymiadou as art designer and additional programmer. The Game Design was collaborated between the three of us. \n\n Note 25/01/2024: this link leads to an old version. The actual final version is being polished and will be updated ASAP.\n\n Password: gooseberry",
     role: "Game Designer, Programmer, Writer",
     
     linkText: "Play it on Itch!",
@@ -128,6 +128,21 @@ let supermarket =
     hasVideo: false,
     
     colour: `url("./images/Supermarket.png")`
+}
+
+let cartoonbase =
+{
+    card: document.getElementById("cbcard"),
+    title:"Cartoonbase",
+    desc: "During my tenure at Cartoonbase, I had multiple functions: copy-/script writer, project management, creative direction,... and more. \n Below you'll find a link to a selection of videos that I worked on in varying capacities - always as writer, but occasionally with extended responsibilities.",
+    role: "Writer and more...",
+    
+    linkText: "Check out my projects!",
+    linkAddress: "https://jonmayes8.wixsite.com/website/work",
+    darkMode:true,
+    hasVideo: false,
+    
+    colour: `url("./images/cbEyes.png")`
 }
 
 let quitenAndTheRabbit =
@@ -149,4 +164,4 @@ illegalHarvest.card.addEventListener('click',function(){OpenWindow(illegalHarves
 thirtyBirds.card.addEventListener('click',function(){OpenWindow(thirtyBirds)})
 quitenAndTheRabbit.card.addEventListener('click',function(){OpenWindow(quitenAndTheRabbit)})
 supermarket.card.addEventListener('click',function(){OpenWindow(supermarket)});
-
+cartoonbase.card.addEventListener('click',function(){OpenWindow(cartoonbase)});
